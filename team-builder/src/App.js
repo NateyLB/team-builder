2dadc3b7cc016328a4f70598af3d9d0eef9ac91a
Nathan <nateyl.b@gmail.com>
2020-03-24 15:25:38 -0700

    Can add a team member

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,23 +1,43 @@
 import React, {useState} from 'react';
+import { v4 as uuid } from 'uuid';
 import logo from './logo.svg';
 import './App.css';
 import TeamList from "./TeamList.js";
+import Form from "./Form.js";
 
 function App() {
 const initialTeam = [
-    {name:"Nathan", email:"Nathan@lambda.com", role:"Back-End Engineer"},
-    {name:"Kyle", email:"Kyle@lambda.com", role:"Font-End Engineer"},
-    {name:"Emily", email:"Emily@lambda.com", role:"UI Engineer"},
-    {name:"Sam", email:"Sam@lambda.com", role:"Mobile Engineer"},
-    {name:"Allen", email:"Allen@lambda.com", role:"Data Scientist"}
+    {id:uuid(), name:"Nathan", email:"Nathan@lambda.com", role:"Back-End Engineer"},
+    {id:uuid(), name:"Kyle", email:"Kyle@lambda.com", role:"Font-End Engineer"},
+    {id:uuid(), name:"Emily", email:"Emily@lambda.com", role:"UI Engineer"},
+    {id:uuid(), name:"Sam", email:"Sam@lambda.com", role:"Mobile Engineer"},
+    {id:uuid(), name:"Allen", email:"Allen@lambda.com", role:"Data Scientist"}
   ];
   
   const [team ,setTeam] = useState(initialTeam);
+  const [name,setName] =useState('');
+  const [email,setEmail] =useState('');
+  const [role,setRole] =useState('');
+
+  const onFormSubmit = event => {
+    // stop the form from reloading the page on submit
+    event.preventDefault()
+    const newMember = {id:uuid(), 
+        name: event.target.name.value, 
+        email: event.target.email.value, 
+        role: event.target.role.value
+    };
+
+    setTeam([...team, newMember]);
+    
+}
+
   console.log(team);
 
   return (
     <div>
       <TeamList data={team}/>
+      <Form onFormSubmit={onFormSubmit}/>
     </div>
   );
 }
---
+++ team-builder/src/Form.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { v4 as uuid } from 'uuid'
+
+function Form(props) {
+const onFormSubmit = props.onFormSubmit;
+    return (
+        <div>
+            <h3>Add a team meber</h3>
+            <form onSubmit={onFormSubmit}>
+        <label>
+            Name:
+        <input
+            type='text'
+            name='name'
+        />
+        </label>
+
+          <label>
+              Email:
+          <input
+              type='text'
+              name='email'
+          />
+          </label>
+
+            <label>
+                Role:
+            <input
+                type='text'
+                name='role'
+            />
+            </label>
+            <input type='submit'/>
+            </form>
+            
+            </div>
+
+    );
+}
+
+export default Form;
